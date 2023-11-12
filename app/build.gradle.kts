6285330a22dbb2bc3dd5a2d4193facf06ca592bf
IgrGabriel <igr1531castro@gmail.com>
2023-11-12 16:26:12 -0300

    adiciona dependencia para coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,6 @@ dependencies {
 
     // Armazenamento - Cloud Store
     implementation("com.google.firebase:firebase-storage-ktx")
-
 }
 
 android {
@@ -68,4 +67,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
 }
