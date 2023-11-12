2b18241433299e846c49e4e0be64228ffbf6146b
IgrGabriel <igr1531castro@gmail.com>
2023-11-12 16:27:31 -0300

    feat: adiciona perfil para o usuario

--- app/src/main/java/com/igorgabriel/recyclerviewtransacoes/TelaPrincipalActivity.kt
+++ app/src/main/java/com/igorgabriel/recyclerviewtransacoes/TelaPrincipalActivity.kt
@@ -1,15 +1,15 @@
 package com.igorgabriel.recyclerviewtransacoes
 
+//import com.igorgabriel.recyclerviewtransacoes.view.MapsFragment
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
 import com.igorgabriel.recyclerviewtransacoes.databinding.ActivityTelaPrincipalBinding
 import com.igorgabriel.recyclerviewtransacoes.view.HomeFragment
-import com.igorgabriel.recyclerviewtransacoes.view.MapsFragment
+import com.igorgabriel.recyclerviewtransacoes.view.ProfileFragment
 import com.igorgabriel.recyclerviewtransacoes.view.TransactionsFragment
 
 class TelaPrincipalActivity : AppCompatActivity() {
@@ -22,10 +22,6 @@ class TelaPrincipalActivity : AppCompatActivity() {
         FirebaseAuth.getInstance()
     }
 
-    private val bancoDados by lazy {
-        FirebaseFirestore.getInstance()
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -34,7 +30,8 @@ class TelaPrincipalActivity : AppCompatActivity() {
             when (it.itemId) {
                 R.id.item_home -> replaceFragment(HomeFragment())
                 R.id.item_transactions -> replaceFragment(TransactionsFragment())
-                R.id.item_map -> replaceFragment(MapsFragment())
+                //R.id.item_map -> replaceFragment(MapsFragment())
+                R.id.item_map -> replaceFragment(ProfileFragment())
                 R.id.item_logout -> confirmLogout()
                 else -> {
                     replaceFragment(HomeFragment())
