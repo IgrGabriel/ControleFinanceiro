12c831d76733c0894c8289aabcd96e118e8da678
IgrGabriel <igr1531castro@gmail.com>
2023-11-12 17:39:59 -0300

    fix: carregar nome do usuário

--- app/src/main/java/com/igorgabriel/recyclerviewtransacoes/view/ProfileFragment.kt
+++ app/src/main/java/com/igorgabriel/recyclerviewtransacoes/view/ProfileFragment.kt
@@ -9,11 +9,6 @@ import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.igorgabriel.recyclerviewtransacoes.R
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class ProfileFragment: Fragment() {
 
@@ -25,7 +20,6 @@ class ProfileFragment: Fragment() {
         FirebaseFirestore.getInstance()
     }
 
-    private var coroutineJob: Job? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -42,32 +36,21 @@ class ProfileFragment: Fragment() {
         val idUsuarioLogado = autenticacao.currentUser?.uid
         val emailUsuarioLogado = autenticacao.currentUser?.email
 
-        if (idUsuarioLogado != null && emailUsuarioLogado != null) {
-            coroutineJob = CoroutineScope(Dispatchers.IO).launch {
-                val refUser = bancoDados.collection("usuarios").document(idUsuarioLogado)
-                var nomeCompleto = ""
-                refUser.addSnapshotListener { documentSnapshot, error ->
-                    if (documentSnapshot != null && documentSnapshot.exists()) {
-                        val dados = documentSnapshot.data
-                        if (dados != null) {
+        if (idUsuarioLogado != null) {
+            val refUser = bancoDados.collection("usuarios").document(idUsuarioLogado)
+            refUser.addSnapshotListener { documentSnapshot, error ->
+                if (documentSnapshot != null && documentSnapshot.exists()) {
+                    val dados = documentSnapshot.data
+                    if (dados != null) {
+                        val nome = dados["nome"].toString()
+                        val sobrenome = dados["sobrenome"].toString()
+                        val nomeCompleto = nome + " " + sobrenome
 
-                            val nome = dados["nome"].toString()
-                            val sobrenome = dados["sobrenome"].toString()
-                            nomeCompleto = nome + " " + sobrenome
-                        }
+                        view?.findViewById<TextView>(R.id.txt_nome_sobrenome)?.text = nomeCompleto
+                        view?.findViewById<TextView>(R.id.text_email)?.text = emailUsuarioLogado
                     }
-                }
-
-                withContext(Dispatchers.Main) {
-                    view?.findViewById<TextView>(R.id.txt_nome_sobrenome)?.text = nomeCompleto
-                    view?.findViewById<TextView>(R.id.text_email)?.text = emailUsuarioLogado.toString()
                 }
             }
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        coroutineJob?.cancel()
     }
 }
